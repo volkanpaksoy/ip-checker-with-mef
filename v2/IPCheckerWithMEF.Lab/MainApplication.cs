ab07737b959f260d4673f4b7229f2fded16ae7a8
volkanp <vpaksoy@rcpsych.ac.uk>
2015-10-20 10:34:45 +0100

    Initial commit - v2

---
+++ v2/IPCheckerWithMEF.Lab/MainApplication.cs
@@ -0,0 +1,39 @@
+﻿using IPCheckerWithMEF.Contract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPCheckerWithMEF.Lab
+{
+    public class MainApplication
+    {
+        private CompositionContainer _container;
+
+        [ImportMany(typeof(IIpChecker))]
+        public List<IIpChecker> IpCheckerList;
+
+        public MainApplication(string pluginFolder)
+        {
+            var catalog = new DirectoryCatalog(pluginFolder);
+            _container = new CompositionContainer(catalog);
+
+            LoadPlugins();
+        }
+
+        public void LoadPlugins()
+        {
+            try
+            {
+                this._container.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+        }
+    }
+}
---
+++ v2/IPCheckerWithMEF.Plugins.DynDnsIPChecker/DynDnsIPChecker.cs
@@ -0,0 +1,40 @@
+﻿using IPCheckerWithMEF.Contract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IPCheckerWithMEF.Plugins.DynDnsIPChecker
+{
+    [Export(typeof(IIpChecker))]
+    public class DynDnsIPChecker : IIpChecker
+    {
+        public string GetExternalIp()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://checkip.dyndns.org");
+                HttpResponseMessage response = client.GetAsync("/").Result;
+                return ExtractIPAddress(response.Content.ReadAsStringAsync().Result);
+            }
+        }
+
+        private string ExtractIPAddress(string fullText)
+        {
+            string regExPattern = @"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b";
+
+            Regex regex = new Regex(regExPattern);
+            Match match = regex.Match(fullText);
+            if (match != null && match.Success)
+            {
+                return match.Value;
+            }
+
+            throw new ArgumentException("Provided text does not contain an IP address");
+        }
+    }
+}
